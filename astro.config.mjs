dc2bbec7c2d172d9231baaab7cb6c3a572f94de7
Arjan Ellingson <arjan.ellingson@gmail.com>
2026-07-07 19:10:50 -0700

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ export default defineConfig({
   site: 'https://aselling.io',
   // No `base` needed if deploying to a custom domain or username.github.io root.
   // If deploying to username.github.io/repo-name instead, uncomment:
-  // base: '/repo-name',
+  base: '/asellingson28.github.io/',
 });
